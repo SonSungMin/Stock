14c475d54737bbc01284d51e80664e267bebdffe
SonSungMin <ssm7777777@gmail.com>
2025-09-30 10:21:52 +0900

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,28 +1,20 @@
 // /netlify/functions/proxy.js
 
-// node-fetch를 import합니다.
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
 exports.handler = async function (event, context) {
     const targetUrl = event.queryStringParameters.targetUrl;
 
     if (!targetUrl) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ error: 'targetUrl 파라미터가 필요합니다.' }),
-        };
+        return { statusCode: 400, body: 'targetUrl 파라미터가 필요합니다.' };
     }
 
     try {
+        // Netlify의 내장 fetch 기능을 바로 사용합니다. (node-fetch 불필요)
         const response = await fetch(decodeURIComponent(targetUrl));
         const data = await response.json();
 
         return {
             statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-            },
+            headers: { 'Access-Control-Allow-Origin': '*' },
             body: JSON.stringify(data),
         };
     } catch (error) {
