64226f005366aae2ea7dc6aaa02761fa08365ce5
SonSungMin <ssm7777777@gmail.com>
2025-10-16 16:08:23 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// js/config.js
+
+// API 키와 프록시 URL 설정
+export const API_KEYS = {
+    FRED: '480b8d74e3d546674e8180193c30dbf6',
+    ECOS: 'C4UHXGGIUUZ1TNZJOXFM'
+};
+export const PROXY_URL = '/.netlify/functions/proxy?targetUrl=';
+export const STOCK_INFO_URL = '/.netlify/functions/stock-info?code=';
+export const STOCK_SEARCH_URL = '/.netlify/functions/stock-list?query=';
