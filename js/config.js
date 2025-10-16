7ae09f2e26235c06f4b80a18a56cf7eb650ad74b
SonSungMin <ssm7777777@gmail.com>
2025-10-16 16:21:33 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,11 @@
 // js/config.js
 
+// ==================================================================
 // API 키와 프록시 URL 설정
+// ==================================================================
 export const API_KEYS = {
-    FRED: '480b8d74e3d546674e8180193c30dbf6',
-    ECOS: 'C4UHXGGIUUZ1TNZJOXFM'
+    FRED: '480b8d74e3d546674e8180193c30dbf6', // 여기에 FRED API 키를 입력하세요.
+    ECOS: 'C4UHXGGIUUZ1TNZJOXFM'  // 여기에 ECOS API 키를 입력하세요.
 };
 export const PROXY_URL = '/.netlify/functions/proxy?targetUrl=';
 export const STOCK_INFO_URL = '/.netlify/functions/stock-info?code=';
